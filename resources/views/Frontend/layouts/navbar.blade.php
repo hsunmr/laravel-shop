72b5a5e6cbef79604d88d92b4077bfb0205e89c9
Hsun <hsunmr0823@gmail.com>
2019-01-25 14:57:32 +0800

    add navbar img and background

--- resources/views/Frontend/layouts/navbar.blade.php
+++ resources/views/Frontend/layouts/navbar.blade.php
@@ -6,24 +6,24 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="nav navbar-nav flex-column mr-auto">
+      <ul class="nav navbar-nav flex-column mr-auto" >
         <li class="nav-item">
           <a class="nav-link text-uppercase font-weight-bold" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-uppercase" href="#">Drink</a>
+        <li class="nav-item ">
+          <a class="nav-link text-uppercase" href="#"><img src="img/icon/drink_icon.png"><span class="align-middle">飲品</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-uppercase" href="#">Food</a>
+          <a class="nav-link text-uppercase" href="#"><img src="img/icon/food_icon.png"><span class="align-middle">食物</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-uppercase" href="#">Product</a>
+          <a class="nav-link text-uppercase" href="#"><img src="img/icon/shop_icon.png"><span class="align-middle">商品</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-uppercase" href="#">About</a>
+          <a class="nav-link text-uppercase" href="#"><img src="img/icon/about_icon.png"><span class="align-middle">關於我們</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-uppercase" href="#">Cart</a>
+          <a class="nav-link text-uppercase" href="#"><img src="img/icon/cart_icon.png"><span class="align-middle">購物車</span></a>
         </li>
       </ul>
     </div>
