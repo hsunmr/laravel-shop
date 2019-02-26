ba601ead135d47214051dc6af683ffea362a10c7
Hsun <hsunmr0823@gmail.com>
2019-02-26 23:30:43 +0800

    add fontawesome resource

--- resources/views/Frontend/layouts/master.blade.php
+++ resources/views/Frontend/layouts/master.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('fontawesome/css/all.css')}}" rel="stylesheet">
+    <link href="{{asset('fontawesome/css/all.min.css')}}" rel="stylesheet">
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
@@ -38,7 +38,7 @@
     
     <script src="{{asset('jquery/jquery.min.js')}}"></script> 
     <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('fontawesome/js/all.js')}}"></script>
+    <script src="{{asset('fontawesome/js/all.min.js')}}"></script>
     <script src="{{asset('js/coffee_shop.js')}}"></script>
 
   </body>
