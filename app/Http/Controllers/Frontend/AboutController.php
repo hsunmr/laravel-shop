06ea9b23ac8b92e6f1201235e017b22604027647
Hsun <hsunmr0823@gmail.com>
2019-03-25 22:58:02 +0800

    link about page with intro and history

--- app/Http/Controllers/Frontend/AboutController.php
+++ app/Http/Controllers/Frontend/AboutController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\About\IntroDiv;
+use App\Models\About\HistoryDiv;
 use App\Models\Share\ShopInfo;
 use App\Models\Share\Calendar;
 use App\Models\Share\Footer;
@@ -11,11 +13,13 @@ class AboutController extends Controller
 {
     public function index(){
         
+        $introdivs = IntroDiv::all();
+        $historys = HistoryDiv::all();
         $shopinfo = ShopInfo::find(1);
         $calendars = Calendar::all();
         $footer = Footer::find(1);
 
-        return view('frontend.about',compact('shopinfo','calendars','footer'));
+        return view('frontend.about',compact('introdivs','historys','shopinfo','calendars','footer'));
 
     }
 }
--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -4,45 +4,32 @@
 <div class="container" id="about_page_content">
     <div class="about_page_des">
         <h2 class="page_title">ABOUT</h2>
+        @foreach ($introdivs as $intro)
         <div class="row about_page_img">
             <div class="col">
-                <img class="d-block w-100 rounded" src="../img/index/about_img.jpg">
+                <img class="d-block w-100 rounded" src="{{asset('uploads/introdiv/' . $intro->image)}}">
             </div>
         </div>
         <div class="row about_page_word">
             <div class="col">
-                <p>
-                    私たちが目指すのは、<br>
-                    たった一杯で幸せになるコーヒー屋。<br>
-                    まずは目の前の人を笑顔にすること、<br>
-                    そのための努力は、惜しみません。<br>
-                    一つの笑顔が連鎖していくように、<br>
-                    世界がワクワクで溢れるまで。
-                </p>
-                <p>
-                    We want to be your coffee shop. A place that brings a smile to your face with just one cup of roasted goodness.<br>
-                    What drives us is bringing joy to each and every person who walks through our doors.<br>
-                    In the hope that your happiness touches others throughout the rest of your day.<br>
-                    Like a chain reaction, building a better community and a better world.
-                </p>
+                {!!$intro->text!!}
             </div>
         </div>
+        @endforeach
+        
     </div>
 
 <div class="container" id="about_page_history">
     <h2 class="page_title">History</h2>
     <div class="row about_history_des">
-        <ul>			
+        <ul>
+            @foreach ($historys as $history)
             <li>
-                <time>2019年2月5日</time>
+                <time>{{$history->created_at->format('Y年m月d日')}}</time>
                 <br class="break">
-                <span class="history_text">OPEN</a></span>
-            </li>
-            <li>
-                <time>2019年2月5日</time>
-                <br class="break">
-                <span class="history_text">OPEN</a></span>
-            </li>
+                <span class="history_text">{{$history->text}}</a></span>
+            </li> 
+            @endforeach		
         </ul>
     </div>
 </div>
